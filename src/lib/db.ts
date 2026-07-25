4083c1d4a16b78b7c5d26be740ae2482f31fbe40
Muhammad Umair <umaair40@gmail.com>
2026-07-25 23:30:25 +0500

    Update just

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client"
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import "dotenv/config";
-
+// Coming from env 
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
